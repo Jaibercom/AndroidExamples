27bcf59d7c21c4fbb3281cfd26d338dfcaac0dec
jaibercom <jaibercom@gmail.com>
2017-03-01 10:39:37 -0500

    Fragment examples

---
+++ Fragments/LifeCycle/src/main/java/co/edu/udea/compumovil/lifecycle/MainActivity.java
@@ -0,0 +1,68 @@
+package co.edu.udea.compumovil.lifecycle;
+
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create new fragment and transaction
+        Fragment fragmentA = new FragmentA();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack
+        transaction.add(R.id.fragment_container, fragmentA);
+
+        // Commit the transaction
+        transaction.commit();
+
+        Log.d(TAG, "onCreate");
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+}
